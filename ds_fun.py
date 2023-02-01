8061a6c4795cd2d607553afc992251578943c021
JoshuaHolt-Tech <116033033+JoshuaHolt-Tech@users.noreply.github.com>
2023-02-01 12:02:27 -0600

    Added function to get wine data.

--- ds_fun.py
+++ ds_fun.py
@@ -17,6 +17,7 @@ warnings.filterwarnings("ignore")
 
 """############## -- AVAILABLE FUNCTIONS -- ##############
 Acquire:
+-pour_wine()
 -wrangle_iris()
 -wrangle_mall()
 -get_telco_data()
@@ -45,6 +46,29 @@ Modeling:
 -scale_cont_columns(train, val, test, scaler_model = 1, cont_columns)
 
 ############## -- ACQUIRE FUNCTIONS -- ##############"""
+
+def pour_wine():
+    """ 
+    This function takes the red and white wine quality csvs, adds a type column and combines into the wine DataFrame.
+    """
+
+    
+    filename = "winequality.csv"
+    
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        red_df = pd.read_csv("winequality-red.csv")
+        white_df = pd.read_csv("winequality-white.csv")
+
+        red_df['type'] = 'red'
+        white_df['type'] = 'white'
+
+        wine_df = pd.concat([red_df, white_df], ignore_index=True)
+
+        wine_df.to_csv(filename, index=False)
+    
+    return wine_df
 
 
 def wrangle_iris():
