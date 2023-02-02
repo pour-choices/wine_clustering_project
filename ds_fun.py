d597287c2fc2f128fa35e0e5a451ae1ebe036f5b
JoshuaHolt-Tech <116033033+JoshuaHolt-Tech@users.noreply.github.com>
2023-02-02 15:08:40 -0600

    Adding current progress for the day.

--- ds_fun.py
+++ ds_fun.py
@@ -12,6 +12,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import RobustScaler, MinMaxScaler, StandardScaler
 from sklearn.metrics import mean_squared_error
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
 
 
 #Removes warnings and imporves asthenics
@@ -741,14 +743,14 @@ def train_val_test(train, val, test, target_col):
     Seperates out the target variable and creates a series with only the target variable to test accuracy.
     """
     #Seperating out the target variable
-    X_train = train.drop(columns=[target])
-    y_train = train[target]
+    X_train = train.drop(columns=[target_col])
+    y_train = train[target_col]
 
-    X_val = val.drop(columns = [target])
-    y_val = val[target]
+    X_val = val.drop(columns = [target_col])
+    y_val = val[target_col]
 
-    X_test = test.drop(columns = [target])
-    y_test = test[target]
+    X_test = test.drop(columns = [target_col])
+    y_test = test[target_col]
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
@@ -815,3 +817,213 @@ def scale_cont_columns(train, val, test, cont_columns, scaler_model = 1):
                                                  columns=test[cont_columns].columns.values).set_index([test.index.values])
     #Sending them back
     return train_scaled, val_scaled, test_scaled
+
+
+
+
+def find_model_scores(df):
+    """
+    This function takes in the target DataFrame, runs the data against four
+    machine learning models and outputs some visuals.
+    """
+    #Creates a copy so the original data is not affected
+    ml_df = df.copy()
+
+    #Drops columns not used in modeling
+    ml_df = df.drop(columns=['transactiondate', 'sqft_bins',
+                             'value_bins', 'County'])
+    #Creates dummy columns
+    ml_df = pd.get_dummies(columns=['bedrooms', 'bathrooms',
+                                    'num_of_features', 'garagecarcnt'],
+                           data=ml_df)
+    #Splits data into train, validate and test datasets
+    train, val, test = train_validate(ml_df)
+    
+    #Scales continuous data#Scaling the data
+    train, val, test = scale_zillow(train, val, test, scaler_model = 3,
+                                    cont_columns = ['sqft'])
+
+    #Seperate target column from feature columns
+    X_train, y_train, X_val, y_val, X_test, y_test = train_val_test(train, val, test, target_col)
+    
+    #Eastablishes the standard to beat
+    baseline = find_baseline(y_train)
+    
+    #List for gathering metrics
+    rmse_scores = []
+
+    
+    """ *** Builds and fits Linear Regression Model (OLS) *** """
+    
+    
+    lm = LinearRegression(normalize=True)
+    lm.fit(X_train, y_train)
+    
+    #Train data
+    lm_preds = pd.DataFrame({'actual':y_train})
+    lm_preds['pred_lm'] = lm.predict(X_train)
+    
+    #Validate data
+    lm_val_preds = pd.DataFrame({'actual':y_val})
+    lm_val_preds['lm_val_preds'] = lm.predict(X_val)
+    
+    #Finds score on Train and Validate data
+    rmse_train = mean_squared_error(lm_preds['actual'],
+                                    lm_preds['pred_lm'],
+                                    squared=False) 
+    rmse_val = mean_squared_error(lm_val_preds['actual'],
+                                  lm_val_preds['lm_val_preds'],
+                                  squared=False) 
+
+    #Adds score to metrics list for later comparison
+    rmse_scores.append({'Model':'OLS Linear',
+                    'RMSE on Train': round(rmse_train,0),
+                    'RMSE on Validate': round(rmse_val,0)})
+    
+    
+    """ *** Builds and fits Lasso Lars Model *** """
+   
+    
+    lars = LassoLars(alpha=.25)
+    lars.fit(X_train, y_train)
+    
+    #Train data
+    ll_preds = pd.DataFrame({'actual':y_train})
+    ll_preds['pred_ll'] = lars.predict(X_train)
+    
+    #Validate data
+    ll_val_preds = pd.DataFrame({'actual':y_val})
+    ll_val_preds['ll_val_preds'] = lars.predict(X_val)
+    
+    #Finds score on Train and Validate data
+    rmse_train = mean_squared_error(ll_preds['actual'],
+                                    ll_preds['pred_ll'],
+                                    squared=False)
+    rmse_val = mean_squared_error(ll_val_preds['actual'],
+                                  ll_val_preds['ll_val_preds'],
+                                  squared=False)
+    
+    #Adds score to metrics list for later comparison
+    rmse_scores.append({'Model':'Lasso Lars',
+                    'RMSE on Train': round(rmse_train,0),
+                    'RMSE on Validate': round(rmse_val,0)})
+    
+    
+    """ *** Builds and fits Tweedie Regressor (GLM) Model *** """
+    
+    glm = TweedieRegressor(power=1, alpha=1)    
+    glm.fit(X_train, y_train)
+
+    #Train data
+    glm_preds = pd.DataFrame({'actual':y_train})
+    glm_preds['pred_glm'] = glm.predict(X_train)
+    
+    #Validate data
+    glm_val_preds = pd.DataFrame({'actual':y_val})
+    glm_val_preds['glm_val_preds'] = glm.predict(X_val)
+    
+    #Finds score on Train and Validate data
+    rmse_train = mean_squared_error(glm_preds['actual'],
+                                    glm_preds['pred_glm'],
+                                    squared=False) 
+    rmse_val = mean_squared_error(glm_val_preds['actual'],
+                                  glm_val_preds['glm_val_preds'],
+                                  squared=False)
+    
+    #Adds score to metrics list for later comparison
+    rmse_scores.append({'Model':'Tweedie',
+                        'RMSE on Train': round(rmse_train,0),
+                        'RMSE on Validate': round(rmse_val,0)})
+    
+    
+    """ *** Builds and fits Polynomial regression Model *** """
+
+    
+    #Polynomial Regression part:
+    # make the polynomial features to get a new set of features
+    pf = PolynomialFeatures(degree=1)
+
+    # fit and transform X_train_scaled
+    X_train_degree2 = pf.fit_transform(X_train)
+
+    # transform X_validate_scaled & X_test_scaled
+    X_validate_degree2 = pf.transform(X_val)
+    X_test_degree2 = pf.transform(X_test)
+
+    #Polynomial Regression being fed into Linear Regression:
+    lm2 = LinearRegression(normalize=True)
+    lm2.fit(X_train_degree2, y_train)
+
+    #Train data
+    lm2_preds = pd.DataFrame({'actual':y_train})
+    lm2_preds['pred_lm2'] = lm2.predict(X_train_degree2)
+
+    #Validate data
+    lm2_val_preds = pd.DataFrame({'actual':y_val})
+    lm2_val_preds['lm2_val_preds'] = lm2.predict(X_validate_degree2)
+
+    #Finds score on Train and Validate data
+    rmse_train = mean_squared_error(lm2_preds['actual'],
+                                    lm2_preds['pred_lm2'],
+                                    squared=False) 
+    rmse_val = mean_squared_error(lm2_val_preds['actual'],
+                                  lm2_val_preds['lm2_val_preds'],
+                                  squared=False)
+
+    #Adds score to metrics list for later comparison
+    rmse_scores.append({'Model':'Polynomial',
+                        'RMSE on Train': round(rmse_train,0),
+                        'RMSE on Validate': round(rmse_val,0)})
+    
+    """ *** Later comparison section to display results *** """
+    
+    #Builds and displays results DataFrame
+    rmse_scores = pd.DataFrame(rmse_scores)
+    rmse_scores['Difference'] = round(rmse_scores['RMSE on Train'] - rmse_scores['RMSE on Validate'],2)    
+    
+    #Results were too close so had to look at the numbers
+    print(rmse_scores)
+    
+    #Building variables for plotting
+    rmse_min = min([rmse_scores['RMSE on Train'].min(),
+                    rmse_scores['RMSE on Validate'].min(), baseline])
+    rmse_max = max([rmse_scores['RMSE on Train'].max(),
+                    rmse_scores['RMSE on Validate'].max(), baseline])
+
+    lower_limit = rmse_min * 0.8
+    upper_limit = rmse_max * 1.05
+
+
+    x = np.arange(len(rmse_scores))  # the label locations
+    width = 0.25  # the width of the bars
+
+    fig, ax = plt.subplots(facecolor="gainsboro")
+    rects1 = ax.bar(x - width/2, rmse_scores['RMSE on Train'],
+                    width, label='Training data', color='#4e5e33',
+                    edgecolor='dimgray') #Codeup dark green
+    rects2 = ax.bar(x + width/2, rmse_scores['RMSE on Validate'],
+                    width, label='Validation data', color='#8bc34b',
+                    edgecolor='dimgray') #Codeup light green
+
+    # Need to have baseline input:
+    plt.axhline(baseline, label="Baseline Error", c='tomato', linestyle=':')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.axhspan(0, baseline, facecolor='palegreen', alpha=0.2)
+    ax.axhspan(baseline, upper_limit, facecolor='red', alpha=0.3)
+    ax.set_ylabel('RMS Error')
+    ax.set_xlabel('Machine Learning Models')
+    ax.set_title('Model Error Scores')
+    ax.set_xticks(x, rmse_scores['Model'])
+
+    plt.ylim(bottom=lower_limit, top = upper_limit)
+
+    ax.legend(loc='upper right', framealpha=.9, facecolor="whitesmoke",
+              edgecolor='darkolivegreen')
+
+    #ax.bar_label(rects1, padding=4)
+    #ax.bar_label(rects2, padding=4)
+    fig.tight_layout()
+    #plt.savefig('best_model_all_features.png')
+    plt.show()
+
